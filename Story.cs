e950e2be109c0e363192f6a5baf9228b705c69e1
Pavithra137 <96110886+Pavithra137@users.noreply.github.com>
2021-12-27 12:57:09 +0530

    Add files via upload

---
+++ Story.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Req4
+{
+    class Story
+    {
+        private string name, authorName, genre;
+        private int noOfChapters, noOfLikes, noOfReads;
+
+        public Story(string name, string authorName, string genre, int noOfChapters, int noOfLikes, int noOfReads)
+        {
+            this.name = name;
+            this.authorName = authorName;
+            this.genre = genre;
+            this.noOfChapters = noOfChapters;
+            this.noOfLikes = noOfLikes;
+            this.noOfReads = noOfReads;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string AuthorName { get => authorName; set => authorName = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public int NoOfChapters { get => noOfChapters; set => noOfChapters = value; }
+        public int NoOfLikes { get => noOfLikes; set => noOfLikes = value; }
+        public int NoOfReads { get => noOfReads; set => noOfReads = value; }
+
+        public override string ToString()
+        {
+            return string.Format(this.name + " " + " " + this.authorName + " " + this.genre
+                + " " + this.noOfChapters + " " + this.noOfLikes + " " + this.noOfReads);
+        }
+
+    }
+}
